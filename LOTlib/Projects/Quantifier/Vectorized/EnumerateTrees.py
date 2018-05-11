d2366b99f3ca91cf49c531682ab39be480102a84
Yen-Ling Kuo <ylkuo@csail.mit.edu>
2018-05-10 22:56:31 -0400

    update tree enumeration

--- LOTlib/Projects/Quantifier/Vectorized/EnumerateTrees.py
+++ LOTlib/Projects/Quantifier/Vectorized/EnumerateTrees.py
@@ -5,26 +5,41 @@ Enumerate all possible trees (as FunctionHypotheses), saving them to a file (for
 
 """
 import sys
+import pickle
 
-from Utilities import *
+from LOTlib.FunctionNode import FunctionNode
+from LOTlib.Hypotheses.LOTHypothesis import LOTHypothesis
+from LOTlib.Miscellaneous import logplusexp
+from LOTlib.TopN import TopN
+from LOTlib.Projects.Quantifier.Model import *
 
-OUT = "data/all_trees_2012May2.pkl"
-DEPTH = 3
-MAX_NODES = 11
+OUT = "data/all_trees_cfg.pickle" #"data/all_trees_csg.pickle"
+DEPTH = 4 #10
+MAX_NODES = 10
 
 all_tree_count = 0
 
 ## Collapse trees by how they act on data -- collapse equivalent functions
 collapsed_forms = dict()
 
+def get_tree_set_responses(t, context_sets):
+    resps = []
+    if isinstance(t, FunctionNode):
+        f = eval('lambda context: ' + str(t))
+    else:
+        f = t.fvalue
+    for context in context_sets:
+        resps.append(f(context))
+    return resps
+
 # A function to collapse trees together based on their functional response
 def add_to_collapsed_trees(t):
-    resps = ';'.join(map(str, get_tree_set_responses(t, all_possible_context_sets)))
-
-    tprior = t.log_probability()
+    resps = ';'.join(map(str, get_tree_set_responses(t, TESTING_SET)))
+    tprior = grammar.log_probability(t)
 
     if resps in collapsed_forms: # add to the existing collapsed form if no recursion
-        collapsed_forms[resps].my_log_probability = logplusexp( collapsed_forms[resps].log_probability(), tprior )
+        collapsed_prob = grammar.log_probability(collapsed_forms[resps])
+        collapsed_forms[resps].my_log_probability = logplusexp(collapsed_prob, tprior)
         if tprior > collapsed_forms[resps].display_tree_probability: # display the most concise form
             collapsed_forms[resps] = t
             collapsed_forms[resps].display_tree_probability = tprior
@@ -36,16 +51,20 @@ def add_to_collapsed_trees(t):
 
 ############################################
 ### Now actually enumarate trees
-for t in grammar.increment_tree('START',DEPTH):
+for t in grammar.enumerate(d=DEPTH):
+    if 'presup_(False' in str(t):
+        continue
+    if not check_expansion(t):
+        continue
     if t.count_subnodes() <= MAX_NODES:
         add_to_collapsed_trees(t)
         all_tree_count += 1
-        print ">", t, t.posterior_score, t.log_probability()
+        print ">", t, grammar.log_probability(t)
 
 ## for kinder saving and unsaving:
-upq = FiniteBestSet(max=True)
+upq = TopN()
 for k in collapsed_forms.values():
-    upq.push(LOTHypothesis(grammar, k, args=['A', 'B', 'S']), 0.0)
-upq.save(OUT)
+    upq.add(LOTHypothesis(grammar, k, display='lambda context: %s'), 0.0)
+pickle.dump(upq, open(OUT, 'w'))
 
 print "Total tree count: ", all_tree_count
