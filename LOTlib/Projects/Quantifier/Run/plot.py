291d3f307d345f7694bea26b4f1a36c90b93810a
Yen-Ling Kuo <ylkuo@csail.mit.edu>
2018-06-19 12:35:11 -0400

    update final plot

--- LOTlib/Projects/Quantifier/Run/plot.py
+++ LOTlib/Projects/Quantifier/Run/plot.py
@@ -1,43 +1,52 @@
 import matplotlib.pyplot as plt
 
-SAMPLE_SIZE = 6000
+SAMPLE_SIZE = {
+    'cfg': 10000,
+    'csg': 10000,
+    'csg_more': 8000
+}
+FOLDER = '' #'/run_0'
+GRAMMARS = ['cfg', 'csg']
 
 cfg_correctness = {
-    'every': [0],
-    'every-prime': [0],
-    'most': [0],
-    'most-prime': [0],
-    'subset': [0],
-    'subset-prime': [0],
-    'differ': [0],
-    'differ-prime': [0],
-    'gleeb': [0],
-    'gleeb-prime': [0]
+    'every': [0 for _ in range(21)],
+    'every-prime': [0 for _ in range(21)],
+    'most': [0 for _ in range(21)],
+    'most-prime': [0 for _ in range(21)],
+    'subset': [0 for _ in range(21)],
+    'subset-prime': [0 for _ in range(21)],
+    'differ': [0 for _ in range(21)],
+    'differ-prime': [0 for _ in range(21)],
+    'gleeb': [0 for _ in range(21)],
+    'gleeb-prime': [0 for _ in range(21)]
 }
 csg_correctness = {
-    'every': [0],
-    'every-prime': [0],
-    'most': [0],
-    'most-prime': [0],
-    'subset': [0],
-    'subset-prime': [0],
-    'differ': [0],
-    'differ-prime': [0],
-    'gleeb': [0],
-    'gleeb-prime': [0]
+    'every': [0 for _ in range(21)],
+    'every-prime': [0 for _ in range(21)],
+    'most': [0 for _ in range(21)],
+    'most-prime': [0 for _ in range(21)],
+    'subset': [0 for _ in range(21)],
+    'subset-prime': [0 for _ in range(21)],
+    'differ': [0 for _ in range(21)],
+    'differ-prime': [0 for _ in range(21)],
+    'gleeb': [0 for _ in range(21)],
+    'gleeb-prime': [0 for _ in range(21)]
 }
 
 def read_correctness(grammar_type):
-    f = open('results/correctness_' + grammar_type + '_' + str(SAMPLE_SIZE) + '.txt', "r")
+    f = open('results'+FOLDER+'/correctness_' + grammar_type + '_' + str(SAMPLE_SIZE[grammar_type]) + '.txt', "r")
     lines = f.readlines()
     for line in lines:
         cols = line.split(',')
         word = cols[0]
-        correctness = float(cols[-3])
+        correctness = float(cols[-6])
+        i = int(cols[1]) / 100
         if grammar_type == 'cfg':
-            cfg_correctness[word].append(correctness)
+            cfg_correctness[word][i] = correctness
         elif grammar_type == 'csg':
-            csg_correctness[word].append(correctness)
+            csg_correctness[word][i] = correctness
+        elif grammar_type == 'csg_more':
+            csg_correctness[word][i] = correctness
     f.close()
 
 def plot(word):
@@ -50,7 +59,7 @@ def plot(word):
     ax.set_xticklabels(['{:4.0f}'.format(x*100) for x in vals])
     fig.savefig('figures/'+word+'.png')
 
-read_correctness('cfg')
-read_correctness('csg')
+for grammar_type in GRAMMARS:
+    read_correctness(grammar_type)
 for word in cfg_correctness.keys():
     plot(word)
